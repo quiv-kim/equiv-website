c77dcda2ac84331ad86962eb0c01144dd3082e93
quiv-kim <salvokim99@gmail.com>
2026-07-29 09:32:37 +0900

    Sprint 8.1 retouch

--- js/consultation-modal.js
+++ js/consultation-modal.js
@@ -38,7 +38,7 @@
           </div>
           <p class="section-kicker">CONSULTATION</p>
           <h2 id="consultation-modal-title">M&amp;A 상담 신청</h2>
-          <p id="consultation-modal-description">상담 내용을 남겨주시면 확인 후 담당자가 연락드립니다.</p>
+          <p id="consultation-modal-description">상담 내용을 남겨주시면 담당자가 확인 후 연락드립니다.</p>
         </header>
 
         <header class="consultation-modal__header consultation-modal__header--success" data-success-header hidden>
